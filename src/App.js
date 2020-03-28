f2ae65d1ed359dc4d2329b4ed3d378d4afa43712
zezeozoz <zeynelozturk91@gmail.com>
2020-03-28 16:05:09 +0100

    shop page added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Switch, Route, Link } from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
     <Switch>
       <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
     </Switch>
     </div>
   );
