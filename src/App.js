8df59b27c500073d2ea119cbb7f7380f9415634e
zezeozoz <zeynelozturk91@gmail.com>
2020-03-28 17:11:17 +0100

    form-input and button components created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route} from 'react-router-dom'
 import './App.css';
+import Header from './components/header/header.component'
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component';
 
 function App() {
   return (
     <div>
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-    </Switch>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.components'
+//import './sign-in-and-sign-up-page.styles.scss';
+
+const SignInAndSignUpPage =() => (
+   <div className='sign-in-and-sign-up-page'>
+      <SignIn />
+   </div>
+)
+
+export default SignInAndSignUpPage;
