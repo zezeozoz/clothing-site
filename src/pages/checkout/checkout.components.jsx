4442f5a254bbd641949699f0d903f671df670dbe
zezeozoz <zeynelozturk91@gmail.com>
2020-04-05 23:50:08 +0200

    advanced routing for categories added

--- src/pages/checkout/checkout.components.jsx
+++ src/pages/checkout/checkout.components.jsx
@@ -10,16 +10,16 @@ import './checkout.styles.scss'
 const CheckoutPage = ({cartItems, total}) => (
    <div className='checkout-page'>
       <div className='checkout-header'>
-         <div classNAme='header-block'>
+         <div className='header-block'>
             <span>Product</span>
          </div>
-         <div classNAme='header-block'>
+         <div className='header-block'>
             <span>Quantity</span>
          </div>
          <div className='header-block'>
             <span>Price</span>
          </div>
-         <div classNAme='header-block'>
+         <div className='header-block'>
             <span>Remove</span>
          </div>
       </div>
